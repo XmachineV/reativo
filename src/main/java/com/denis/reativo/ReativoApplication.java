e4f7462b3ccdba0487347087b7846c72c2f83cc3
Denis Baroni <denis.baroni@outlook.com>
2019-05-12 11:04:36 -0300

    Primeiro Commit

---
+++ src/main/java/com/denis/reativo/ReativoApplication.java
@@ -0,0 +1,13 @@
+package com.denis.reativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReativoApplication.class, args);
+	}
+
+}
